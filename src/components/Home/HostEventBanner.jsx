434123570a6c17e99544b5ee9033020476dec53d
Niva02M <nivv2059@gmail.com>
2024-05-14 18:16:44 +0545

    added host events banner

---
+++ src/components/Home/HostEventBanner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../css/hostevents.css";
+const HostEventBanner = () => {
+  return (
+    <>
+      <div className="host-event">
+        <h1>Want to host an Event??</h1>
+        <button className="host-button">Host Event</button>
+      </div>
+    </>
+  );
+};
+
+export default HostEventBanner;
