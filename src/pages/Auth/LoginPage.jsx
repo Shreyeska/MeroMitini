86632ee1cc99006e8e86273b2263191542e8c37e
DRI <dristimaharjan7@gmail.com>
2024-05-15 07:27:48 +0545

    html and css of login pages

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -3,7 +3,28 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <h1>This is the Login Page</h1>
+    {/* mailey html and css garey, react aayenw malai */}
+      {/* <!DOCTYPE html> */}
+{/* <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+    <form id="loginForm" action="/login" method="POST">
+        <h2>Login</h2>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <button type="submit">Login</button>
+        <p>New user? <a href="signup.html">Signup here</a></p>    </form>
+</body>
+</html> */}
+
+      
     </>
   );
 };
--- src/pages/Auth/SignupPage.jsx
+++ src/pages/Auth/SignupPage.jsx
@@ -8,6 +8,33 @@ const SignUp = () => {
       <Navbar />
       <h1>This is the Sign up Page</h1>
       <Footer />
+      {/* <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+    <form id="signupForm" action="/register" method="POST">
+        <h2>Sign Up</h2>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="username" required>
+        <label for="number">Number:</label>
+        <input type="text" id="number" name="number" required>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <label for="Ward no">Ward no:</label>
+        <input type="text" id="wardNumber" name="wardNumber" required>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <button type="submit">Sign Up</button>
+        <p>Already have an account? <a href="login.html">Login here</a></p>
+    </body>
+
+</html>
+ */}
     </>
   );
 };
